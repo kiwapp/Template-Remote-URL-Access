3c8e1a551876d749f416509a80b20d2f05848435
Aldorus <guillaume@procheo.fr>
2015-01-08 14:02:55 +0100

    CUST-90 update process + add source to app project

---
+++ app-setup/GulpConfig.js
@@ -0,0 +1,62 @@
+/**
+ * The url for the api Kiwapp you want use
+ * @type {string}
+ */
+var urlApi = "https://api.kiwapp.com/nosecure/appInstance/";
+/**
+ * The application token for this application
+ * You must have manualy uploaded on the Manager once
+ * After that you find the application token in the interface
+ * @type {string}
+ */
+var appToken = "123456";
+/**
+ * The destination folder you want yours built resources have been
+ * @type {string}
+ */
+var dist = "../build/";
+
+/**
+ * The project sources folders
+ * @type {string}
+ */
+var project = "../";
+
+/**
+ * The application name
+ * @type {string}
+ */
+var appName = "kiwappSetup";
+
+var jsVendors = [
+    project + 'src/vendor/angular/angular.min.js',
+    project + 'src/vendor/angular-animate/angular-animate.min.js',
+    project + 'src/vendor/angular-sanitize/angular-sanitize.min.js',
+    project + 'src/vendor/angular-touch/angular-touch.min.js',
+    project + 'src/vendor/angular-ui-router/release/angular-ui-router.min.js',
+    project + 'src/vendor/moment/moment.js',
+    project + 'src/vendor/KiwappAPI_AccessLib/dist/bundle.min.js',
+    project + 'src/vendor/pace/pace.min.js',
+    project + 'src/vendor/ngBabelfish/dist/bundle.js'
+];
+
+var cssVendors = [
+    project + 'src/vendor/bootstrap/dist/css/bootstrap.min.css'
+];
+
+/**
+ * The default port where the livereload application is launched
+ * @type {number}
+ */
+var defaultPort = 8080;
+
+module.exports = {
+    urlApi: urlApi,
+    appToken: appToken,
+    dist: dist,
+    project: project,
+    jsVendors: jsVendors,
+    cssVendors: cssVendors,
+    appName: appName,
+    defaultPort: defaultPort
+}
---
+++ app/src/scripts/app.js
@@ -0,0 +1,31 @@
+/**
+ * The web app
+ */
+(function () {
+
+    'use strict';
+
+    var urlConfig = "../config/kiwapp_config.js";
+
+    // Load the configuration for kiwapp (this configuration is generated is store on the device)
+    new Kiwapp(urlConfig, function () {
+        // Notify to the drive Kiwapp application (iOS or Android) : The DOM is ready (so we remove the device loader)
+        Kiwapp.ready();
+        // Get from shop data orientation and url values for bloc natif rotation and load specific url
+        var appInstanceData = Kiwapp.get('instanceParameters')['app-params-remote-url'];
+        var orientation = appInstanceData.orientation;
+        var url = appInstanceData.url;
+        var scrollState = appInstanceData.scroll;
+        // bloc device's rotation
+        Kiwapp.rotate(orientation);
+        Kiwapp.scroll(!!scrollState);
+        // load url
+        if (url !== "" && url !== undefined) {
+            window.location.replace(url);
+        } else {
+            document.body.classList.add("no-Url");
+        }
+    });
+
+})(window, window.document, window.app || (window.app = {}));
+
