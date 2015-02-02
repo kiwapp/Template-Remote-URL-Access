e06d88ebfeb01bc3972bbdc3d64f0cfd76180465
Aldorus <guillaume@procheo.fr>
2015-02-02 10:12:17 +0100

    CUST-221 add zip

--- app/GulpConfig.js
+++ app/GulpConfig.js
@@ -23,20 +23,44 @@ var dist = "../build/";
 var project = "../";
 
 /**
- * The application name
+ * The application name (in min case)
  * @type {string}
  */
 var appName = "Favorite Website";
 
+/**
+ * List the project dependecies you want build
+ * Is generally the microbackoffice dependencies
+ * The order is important
+ * @type {Array}
+ */
+var projectsDependencies = [
+    {
+        'project' : '../../app-setup/GulpFile.js',
+        'task': 'prod'
+    }
+];
+
+/**
+ * List all libraries js dependencies
+ * They will be concat into a vendor.js in the build/js folder
+ * The link to vendor.js is already write into the index.html file
+ * @type {*[]}
+ */
 var jsVendors = [
     project + 'src/vendor/kiwapp.js/kiwapp.js'
 ];
 
+/**
+ * List all libraries css dependencies,
+ * They will be copied in the build/style folder so you must make a link to them into your index.html file
+ * @type {*[]}
+ */
 var cssVendors = [
 ];
 
 /**
- * The default port where the livereload application is launched
+ * The default port where the application is launched
  * @type {number}
  */
 var defaultPort = 8080;
@@ -46,8 +70,9 @@ module.exports = {
     appToken: appToken,
     dist: dist,
     project: project,
+    projectsDependencies: projectsDependencies,
     jsVendors: jsVendors,
     cssVendors: cssVendors,
     appName: appName,
     defaultPort: defaultPort
-}
+};
